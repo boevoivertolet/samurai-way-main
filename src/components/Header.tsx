68e588f94cabb40f2edeb03f7486cd3aac6a6fa2
boevoiverolet <bjha78rus@yandex.ru>
2022-04-05 13:27:26 +0300

    add components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    </header>
+}
+
+export default Header;
