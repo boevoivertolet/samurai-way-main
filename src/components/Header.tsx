e5b8766a075d83fbb90866aea9b90f6f08479469
boevoiverolet <bjha78rus@yandex.ru>
2022-04-05 13:28:33 +0300

    add components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import './Header.css';
 const Header = () => {
     return <header className='header'>
         <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+
     </header>
 }
 
