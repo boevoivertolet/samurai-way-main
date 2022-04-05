c252df456b448c1e860e5e42d78a014ba8fe35aa
boevoiverolet <bjha78rus@yandex.ru>
2022-04-05 16:19:01 +0300

    //присваиваем 2 класса 1 дивке <div className={`${c.item} ${c.active}`}>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import c from './Header.module.css';
 
 
-
 const Header = () => {
     return <header className={c.header}>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png'/>
     </header>
 }
 
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import c from './Profile.module.css';
 
 
-
-
-
 const Profile = () => {
     return <div className={c.content}>
         <div>
