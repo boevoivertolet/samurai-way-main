ccd1c9c14f764cb89d2f5b30d660fa1fbba0a5b8
boevoiverolet <bjha78rus@yandex.ru>
2022-04-09 17:40:24 +0300

    Add Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,44 @@ import c from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div className={c.content}>
-            Dialogs
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <div className={c.dialog +''+c.active}>
+                    Ibrahim
+                </div>
+                <div className={c.dialog}>
+                    Muhammad
+                </div>
+                <div className={c.dialog}>
+                    Xattab
+                </div>
+                <div className={c.dialog}>
+                    Osama
+                </div>
+                <div className={c.dialog}>
+                    Suleyman
+                </div>
+                <div className={c.dialog}>
+                    Saddam
+                </div>
+                <div className={c.dialog}>
+                    Hadji
+                </div>
+                <div className={c.dialog}>
+                    Djohar
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>hi</div>
+                <div className={c.message}>hello</div>
+                <div className={c.message}>how are you?</div>
+                <div className={c.message}>what is love?</div>
+                <div className={c.message}>Baby dont heart me</div>
+                <div className={c.message}>Dont heart me</div>
+                <div className={c.message}>No more..</div>
+                <div className={c.message}>Vladislav!</div>
+
+            </div>
         </div>
     )
 
